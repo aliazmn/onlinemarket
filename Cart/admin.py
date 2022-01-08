cdc4e0ca48b26610e9484e65148d20c346419715
ali <aliazamian1@gmail.com>
2022-01-08 13:39:32 +0330

    adminregister

--- Cart/admin.py
+++ Cart/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CartMe, CartItem
+
+
+class CartItemInline(admin.StackedInline):
+    model = CartItem
+
+
+@admin.register(CartMe)
+class CartModelAdmin(admin.ModelAdmin):
+    inlines = [CartItemInline]
+
--- User/admin.py
+++ User/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile,Admin,Customer,SalesMan
 
-# Register your models here.
+
+
+admin.site.register(Profile)
+admin.site.register(Admin)
+admin.site.register(Customer)
+admin.site.register(SalesMan)
+
