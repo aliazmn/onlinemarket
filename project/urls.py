a118b0fac5186d1b7581fc0daedd7ae2aa1b17e1
asal <asalhnzd.1995@gmail.com>
2022-01-12 20:03:59 +0330

    fix conflict

--- project/urls.py
+++ project/urls.py
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", index, name="home"),
-    path("", include("User.urls", namespace='user')),
-
-    
-=======
 
 from django.urls.conf import include
 from .views import Home, searchbox
@@ -18,6 +8,11 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+
+    
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('search/',searchbox, name='serachbox'),
@@ -25,9 +20,9 @@ urlpatterns = [
     path('pro/',include('Product.urls')),
     path('comment/',include('Comment.urls', namespace='comment')),
     path('product/',include("Product.urls",namespace="product") ),
+    path("", include("User.urls", namespace='user')),
 
 
->>>>>>> 1be6c836f417ba9a945f338c8dee82dda93cf4a6
 ]
 
 
