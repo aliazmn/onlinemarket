f1ecd90463874b14478a32233436ed8937dd08b0
asal <asalhnzd.1995@gmail.com>
2022-02-15 21:05:17 +0330

    Dockerise project complet

--- project/celery.py
+++ project/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-app = Celery('porject')
+app = Celery('project')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
